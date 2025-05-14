26057f5f85cc29a7414f6671d865ffa0209f1847
Amit Jotwani <hello@ajot.me>
2025-05-14 17:52:03 -0400

    fixing rack protection issues

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 
 # disable the new HostAuthorization middleware entirely
-set :host_authorization, { permitted_hosts: [] }
+# set :host_authorization, { permitted_hosts: [] }
+set :host_authorization, { permitted_hosts: [ ENV.fetch('APP_DOMAIN'), 'localhost' ] }
 
 set :port, ENV.fetch('PORT', 8080)
 set :bind, '0.0.0.0'
